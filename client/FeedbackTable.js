f05da0f38f29fa1d9e644a386b2380c0586b6ed1
Carlos Saito <carlos@saito.style>
2019-12-16 23:07:00 +0100

    Move things to the frontend

--- client/FeedbackTable.js
+++ client/FeedbackTable.js
@@ -5,15 +5,14 @@ function FeedbackTable ({ data }) {
     .slice()
     .sort((a, b) => a.name.localeCompare(b.name, 'sv'))
 
-  const changed = data.filter(r => r.newGrade)
+  const success = data.filter(r => r.success)
+  const failed = data.filter(r => !r.success)
 
   return (
     <table border='1'>
       <caption>
-        <p>
-          Grades of {changed.length}/{sortedList.length} students have been
-          updated
-        </p>
+        <p>Grades of {success.length} students have been updated.</p>
+        <p>{failed.length} failed</p>
       </caption>
       <thead>
         <tr>
@@ -25,8 +24,9 @@ function FeedbackTable ({ data }) {
         {sortedList.map((row, i) => (
           <tr key={i}>
             <td>{row.name}</td>
-            <td>{row.newGrade}</td>
-            <td>{row.newGrade ? 'Has been updated' : ''}</td>
+            <td>
+              {row.success ? `Updated grade to "${row.ladokGrade}"` : 'Failed'}
+            </td>
           </tr>
         ))}
       </tbody>
