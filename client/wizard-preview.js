7879b087cee41a362363003a02eb1c34b5f4f25d
Carlos Saito <carlos@saito.style>
2020-05-07 08:52:52 +0200

    Change button position under the table

--- client/wizard-preview.js
+++ client/wizard-preview.js
@@ -3,24 +3,26 @@ import Table from './table'
 
 function TableFooter ({ onBack, onSubmit, onCancel }) {
   return (
-    <div className='button-section'>
-      <button
-        type='button'
-        className='btn btn-back btn-secondary grid-col-1'
-        onClick={onBack}
-      >
-        Assignments and Date
-      </button>
-      <button
-        type='button'
-        className='btn btn-secondary grid-col-2'
-        onClick={onCancel}
-      >
-        Cancel
-      </button>
-      <button className='btn btn-primary grid-col-3' onClick={onSubmit}>
-        Transfer to Ladok
-      </button>
+    <div className='fixed-bottom button-section-container'>
+      <div className='button-section'>
+        <button
+          type='button'
+          className='btn btn-back btn-secondary grid-col-1'
+          onClick={onBack}
+        >
+          Assignments and Date
+        </button>
+        <button
+          type='button'
+          className='btn btn-secondary grid-col-2'
+          onClick={onCancel}
+        >
+          Cancel
+        </button>
+        <button className='btn btn-primary grid-col-3' onClick={onSubmit}>
+          Transfer to Ladok
+        </button>
+      </div>
     </div>
   )
 }
