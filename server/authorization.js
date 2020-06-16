a33e5c0ceb6778f60bf8f18ed77dce722e94adae
Carlos Saito <carlos@saito.style>
2020-06-16 14:45:32 +0200

    Enhance error message for denyActAs

--- server/authorization.js
+++ server/authorization.js
@@ -1,15 +1,14 @@
-const log = require('skog')
-const { ClientError } = require('../lib/errors')
-
 async function denyActAs (req, res, next) {
   const accessData = req.accessData || req.signedCookies.access_data
 
   if (accessData.realUserId && accessData.userId !== accessData.realUserId) {
-    throw new ClientError(
-      'not_allowed',
-      'You are not allowed to use this app in Masquerade mode ("acting as" a different user)'
-    )
+    res.status(400).send({
+      message: 'You cannot send grades in masquerade ("act as") mode'
+    })
+
+    return
   }
+
   next()
 }
 
