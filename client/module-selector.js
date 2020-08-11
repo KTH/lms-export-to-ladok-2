34f4a0e2b320cf6e9d984af610fcc158eec365d4
Johan Öhman Saldes <johan.saldes@gmail.com>
2020-08-11 13:59:55 +0200

    updated module id

--- client/module-selector.js
+++ client/module-selector.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 /**
  * Component to choose one module
  * - modules. list of "modules" the user can choose from
@@ -7,6 +7,27 @@ import React from 'react'
  * - value. Index on the list to show as selection
  */
 export default function ModuleSelector ({ modules, onChange, value }) {
+  useEffect(() => {
+    if (modules.length === 1) {
+      onChange(0)
+    }
+  })
+  if (modules.length === 1) {
+    return (
+      <div className='select-wrapper'>
+        <select
+          className='custom-select'
+          name='ladok_module'
+          value={value}
+          disabled
+        >
+          <option value={-1} disabled hidden>
+            {modules[0].code} - ({modules[0].name.en} / {modules[0].name.sv})
+          </option>
+        </select>
+      </div>
+    )
+  }
   return (
     <div className='select-wrapper'>
       <select
@@ -18,11 +39,14 @@ export default function ModuleSelector ({ modules, onChange, value }) {
         <option value={-1} disabled hidden>
           Select Ladok module
         </option>
-        {modules.map((module, i) => (
-          <option key={i} value={i}>
-            {module.code} - ({module.name.en} / {module.name.sv})
-          </option>
-        ))}
+        {modules.map((module, i) => {
+          console.log(i)
+          return (
+            <option key={i} value={i}>
+              {module.code} - ({module.name.en} / {module.name.sv})
+            </option>
+          )
+        })}
       </select>
     </div>
   )
