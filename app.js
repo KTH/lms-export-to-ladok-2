4175bf9cddef9740cf96f55ef8beaa6d10320650
Carlos Saito <carsai@kth.se>
2021-01-13 14:11:17 +0100

    Add uncaughtException check

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ require('skog/bunyan').createLogger({
 })
 require('./lib/mongo').init()
 
-const skog = require('skog')
+const log = require('skog')
 const server = require('./server')
 
+process.on('uncaughtException', err => {
+  log.fatal(err, 'Uncaught Exception thrown')
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (reason, p) => {
+  throw reason
+})
+
 server.listen(process.env.PORT || 3001, async () => {
-  skog.info(
+  log.info(
     'Server started. Go to http://localhost:%s',
     process.env.PORT || 3001
   )
